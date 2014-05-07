e8185830189e2b646d56076e2cc7668b2a98ac6f
abailey7073 <abailey7073@sohamcollege.org.uk>
2014-05-07 09:39:39 +0100

    Green Bottles Challenge (ABailey7073 Patch 1.2)

--- greenBottles.py
+++ greenBottles.py
@@ -3,5 +3,5 @@ y = int(input("How many green bottles?"))
 while y>0:
     print("{0} green bottles hanging on the wall.\n{0} Green bottles hanging on the wall.".format(y))
     print("And if one green bottle should accidentally fall,".format(y))
-    print("There'll be {1} green bottles hanging on the wall.".format(y))
     y=y-1
+    print("There'll be {0} green bottles hanging on the wall.".format(y))
