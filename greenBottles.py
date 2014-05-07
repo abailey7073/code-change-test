b54e5308412f0474ae629776a84a5626acd79749
abailey7073 <abailey7073@sohamcollege.org.uk>
2014-05-07 09:34:42 +0100

    Green Bottles  Challenge (ABailey7073 patch 1.10)

--- greenBottles.py
+++ greenBottles.py
@@ -1,10 +1,7 @@
 #Green bottles challenge
-import time
 y = int(input("How many green bottles?"))
 while y>0:
     print("{0} green bottles hanging on the wall.\n{0} Green bottles hanging on the wall.".format(y))
     print("And if one green bottle should accidentally fall,".format(y))
     print("There'll be {1} green bottles hanging on the wall.".format(y))
-    print("")
-    time.sleep(0.5)
     y=y-1
